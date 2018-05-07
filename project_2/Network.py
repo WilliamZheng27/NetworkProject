2e3ae16737a7b41bad70aabff8a771b77ee9313e
WilliamZheng27_Deepin <zyx19981010@163.com>
2018-05-07 13:59:38 +0800

    Some small changes
    Stop tracking Project 1

--- project_2/Network.py
+++ project_2/Network.py
@@ -1,3 +1,5 @@
+#-*- coding:utf-8 -*-
+
 import socket
 import threading
 max_listen_num = 5
@@ -5,12 +7,16 @@ request_len = 1024
 
 
 class Network:
-    def __init__(self,network_send_port,network_recv_port,is_running = 1):
+    def __init__(self, network_send_port, network_recv_port):
         self.send_port = network_send_port
         self.recv_port = network_recv_port
-        self.ip_addr = self.get_host_ip()
-        if is_running:
-            self.start_listen()
+        self.source_ip = self.get_host_ip()
+        self.sock_send = socket.socket()
+        self.send_status = 0
+        self.target_ip = ''
+        self.target_port = 0
+        self.sock_recv = socket.socket()
+        self.recv_status = 0
 
     def get_host_ip(self):
         try:
@@ -21,33 +27,120 @@ class Network:
             s.close()
         return ip
 
-    def start_listen(self):
-        self.sock_recv = socket.socket()
-        self.sock_recv.bind((self.ip_addr,self.recv_port))
+    def connect(self,target_ip,target_port):
+        if self.send_status:
+            raise Exception,'Already connected'
+        self.sock_send.connect((target_ip,target_port))
+        self.send_status = 1
+
+    def disconnect(self):
+        if not self.send_status:
+            raise Exception,'Not connected'
+        self.sock_send.close()
+        self.send_status = 0
+
+    def start_listen(self, call_back_request_handler):
+        if self.recv_status:
+            raise Exception,'Already listening'
+        self.sock_recv.bind((self.source_ip, self.recv_port))
         self.sock_recv.listen(max_listen_num)
-        t = threading.Thread(target=self.thread_accept)
+        t = threading.Thread(target=self.__thread_accept,args=[call_back_request_handler])
         t.start()
 
     def stop_listen(self):
+        if not self.recv_status:
+            raise Exception,'Not listening'
         self.sock_recv.close()
 
-    def __requestHandler(self):
-        data = self.recv(self.sock_recv,request_len)
-        
+    def request(self, target_ip, target_port, method, keep_alive, data=''):
+        if not self.send_status:
+            self.connect(target_ip,target_port)
+        pkg = self.__pack_request(method,self.target_ip,self.target_port,len(data),keep_alive,data)
+        self.__send(self.sock_send, pkg)
+        respose = b''
+        respose += self.recieve(self.sock_send,request_len)
+        respose = self.__unpack_request(respose)
+        body_len = int(respose[6])
+        body_buff = self.recieve(self.sock_send,body_len)
+        body_buff = body_buff.decode()
+        respose += body_buff
+        if not keep_alive:
+            self.sock_send.close()
+        return respose
 
-    def thread_accept(self):
+
+    def __requestHandler(self,call_back_handler):
+        data = b''
+        data += self.recieve(self.sock_recv,request_len)
+        data = data.decode()
+        data = data.split('\r\n')
+        ret = data
+        keep_alive = int(data[5])
+        body_len = int(data[6])
+        data = b''
+        data += self.recieve(self.sock_recv, body_len)
+        body = data.decode()
+        ret += body
+        call_back_handler(ret)
+        if not keep_alive:
+            self.sock_recv.close()
+
+    def __thread_accept(self,call_back_request_handler):
         while True:
             source_ip,source_port = self.sock_recv.accept()
-            t = threading.Thread(target=self.__requestHandler)
+            t = threading.Thread(target=self.__requestHandler,args=[call_back_request_handler])
             t.start()
-    def send(self,target_ip,target_port,data,is_need_response = 0):
-        self.sock_send = socket.socket()
-        self.sock_send.connect((target_ip,target_port))
-        self.sock_send.send(data)
-        if not is_need_response:
-            self.sock_send.close()
 
-    def recv(self,sock,data_size):
+    def __pack_request(self,method,target_ip,target_port,body_len,keep_alive,body):
+        respose = ''
+        respose += str(method)
+        respose += '\r\n'
+        respose += self.source_ip
+        respose += '\r\n'
+        respose += str(self.send_port)
+        respose += '\r\n'
+        respose += target_ip
+        respose += '\r\n'
+        respose += str(target_port)
+        respose += '\r\n'
+        respose += str(keep_alive)
+        respose += '\r\n'
+        respose += str(body_len)
+        respose += '\r\n'
+        respose += body
+        respose += '\r\n'
+        respose = respose.encode()
+        return respose
+    #TODO
+    def __unpack_request(self,buffer):
+        buffer.decode()
+        buffer = buffer.spilt('\r\n')
+        return buffer
+
+
+    def __pack_respond(self,status_code,target_ip,target_port,body_len,body):
+        respose = ''
+        respose += str(status_code)
+        respose += '\r\n'
+        respose += self.source_ip
+        respose += '\r\n'
+        respose += str(self.send_port)
+        respose += '\r\n'
+        respose += target_ip
+        respose += '\r\n'
+        respose += str(target_port)
+        respose += '\r\n'
+        respose += str(body_len)
+        respose += '\r\n'
+        respose += body
+        respose += '\r\n'
+        respose = respose.encode()
+        return respose
+
+    def __send(self,sock,data):
+        sock.send(data)
+
+    def recieve(self,sock,data_size):
         buffer = b''
         while len(buffer) < data_size:
             buffer += sock.recv(1024)
--- project_2/Router.py
+++ project_2/Router.py
@@ -1,16 +1,56 @@
+#-*- coding:utf-8 -*-
+
+import Network
+
+
 class Router:
-    def __init__(self,router_id,router_routingTable):
+    def __init__(self,router_id,send_port,recv_port,router_routingTable=None):
         self.id = router_id
         self.routingTable = router_routingTable
-    def add(self,router_new):
-        self.routingTable += router_new
-        self.renew()
+        self.network_obj = Network.Network(send_port,recv_port)
+
+
+    def initialize(self, method = 0):
+
+
     #TODO:刷新路由表
-    def renew(self):
-        #some code
+    '''
+    Method=0:LS算法
+    Method=1:DV算法
+    '''
+    def renew(self, neighbors,  method=0):
+
+        if method:
+            self.LS(neighbors, route_table_dict)
+        return
     #TODO:断开路由
     def disconnect(self):
-        #some code
+        return
+    #TODO:转发数据包
+    def routing(self, pkg):
+        return
+    #TODO:LS算法
+    def LS(self, neighbor_distance_dict, route_table_dict):
+        '''
+        :param neighbor_distance_dict:字典结构，key为邻居router_id, value为距离
+        :param route_table_dict:字典结构，key为邻居router_id, value为字典{key为目的router_id, value为链路长度}
+        '''
+        neighbor = []
+        for key in neighbor_distance_dict:
+            neighbor.append(key)
+
+        self.routingTable[neighbor[0]] = (neighbor_distance_dict[neighbor[0]], neighbor[0])
+        for key, value in route_table_dict[neighbor[0]].items():
+            self.routingTable[key] = (neighbor_distance_dict[neighbor[0]] + value, neighbor[0])
+
+        for i in range(1, len(neighbor)):
+            if neighbor_distance_dict[neighbor[i]] < self.routingTable[neighbor[i]][0]:
+                self.routingTable[neighbor[i]] = (neighbor_distance_dict[neighbor[i]], neighbor[i])
+            for key, value in route_table_dict[neighbor[i]].items():
+                if self.routingTable[key][0] > value + neighbor_distance_dict[neighbor[i]]:
+                    self.routingTable[key] = (value + neighbor_distance_dict[neighbor[i]], neighbor[i])
+#TODO:DV算法
+
 
 
 
